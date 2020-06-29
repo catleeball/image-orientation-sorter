25607bff307c5f8cc080e88823d43e3501630b77
🎷🐛 Lee Ball <lee.cat.ball@gmail.com>
2020-06-28 18:34:41 -0700

    Use structopt for arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,26 @@
-use clap::{App, Arg, crate_version};
+use std::path::PathBuf;
+use structopt::StructOpt;
 
-fn main() {
-    let args = get_args();
-    let imgdir: &str = args.value_of("Directory").unwrap_or("./");
-    let recursive: bool = args.value_of("Recursive").unwrap_or("false").parse().unwrap();
-    let mv: bool = args.is_present("Move");
-    let prepend: &str = args.value_of("Prepend").unwrap_or("");
-    let print: bool = args.is_present("Print");
-    let verbose: bool = args.is_present("Verbose");
-    let dryrun: bool = args.is_present("Dry Run");
-    println!("{}\n{}\n{}\n{}\n{}\n{}\n{}", imgdir, recursive, mv, prepend, print, verbose, dryrun);
+#[derive(Debug, StructOpt)]
+#[structopt(name = "imgorisort", about = "Image Orientation Sorter")]
+struct Opt {
+    #[structopt(parse(from_os_str), help = "Directory containing image files to sort by orientation.")]
+    directory: PathBuf,
+    #[structopt(short, long, help = "Recurse into subdirectories.")]
+    recursive: bool,
+    #[structopt(short = "m", long = "move", help = "Directory to move images into, sorted by orientation. Subdirectories [portrait, landscape, square] may be created in this directory.")]
+    mv: bool,
+    #[structopt(short, long, help = "Prepend 'portrait', 'landscape', or 'square' to image filenames.")]
+    prefix: bool,
+    #[structopt(short, long, help = "Print source path, destination path, and orientation of each file.")]
+    verbose: bool,
+    #[structopt(short, long, help = "Do not print anything to stdout. Errors may still appear in stderr.")]
+    quiet: bool,
+    #[structopt(short, long, help = "Do not actually move any files. Implies verbose unless --quiet is provided.")]
+    dry_run: bool,
 }
 
-/// Return CLI arguments.
-pub fn get_args() -> clap::ArgMatches<'static> {
-
-    return App::new("imgorisort")
-        .about("Image Orientation Sorter")
-        .version(crate_version!())
-        .arg(Arg::with_name("Directory")
-            .help("Directory containing image files to sort by orientation.")
-            .index(1)
-            .required(true)
-            .default_value("./"))
-        .arg(Arg::with_name("Recursive")
-            .help("Recurse into subdirectories.")
-            .long("recursive")
-            .short("r")
-            .takes_value(false)
-            .default_value("false"))
-        .arg(Arg::with_name("Move")
-            .help("Directory to move images into, sorted by orientation. Subdirectories [portrait, landscape, square] may be created in this directory.")
-            .long("move")
-            .short("m")
-            .takes_value(true)
-            .default_value("./"))
-        .arg(Arg::with_name("Prepend orientation to filename")
-            .help("Prepend 'portrait', 'landscape', or 'square' to image filenames.")
-            .long("prepend")
-            .short("p")
-            .takes_value(false)
-            .default_value("false"))
-        .arg(Arg::with_name("Print orientations")
-            .help("Print to stdout each filename and its orientation")
-            .long("print")
-            .short("p")
-            .takes_value(false)
-            .default_value("true"))
-        .arg(Arg::with_name("Verbose")
-            .help("Print absolute source path and destination path of each file")
-            .long("verbose")
-            .short("v")
-            .takes_value(false)
-            .default_value("false"))
-        .arg(Arg::with_name("Dry Run")
-            .help("Do not actually move any files. Implies --print and --verbose.")
-            .long("dry-run")
-            .short("d")
-            .takes_value(false)
-            .default_value("false"))
-        .get_matches();
+fn main() {
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
 }
