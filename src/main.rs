3e4ea071469c50ab5b609d781200a47d18c7d818
🎷🐛 Lee Ball <lee.cat.ball@gmail.com>
2020-06-28 22:12:10 -0700

    Move init logic, refactor input opts

--- src/main.rs
+++ src/main.rs
@@ -12,36 +12,30 @@ use walkdir::WalkDir;
 #[structopt(name = "imgorisort", about = "Image Orientation Sorter")]
 struct Opt {
     #[structopt(parse(from_os_str), help = "Directory containing image files to sort by orientation.")]
-    directory: PathBuf,
+    input_dir: PathBuf,
+    #[structopt(parse(from_os_str), help = "Directory to output sorted images into")]
+    output_dir: PathBuf,
     #[structopt(short, long, help = "Recurse into subdirectories.")]
     recursive: bool,
-    #[structopt(short = "m", long = "move", help = "Directory to move images into, sorted by orientation. Subdirectories [portrait, landscape, square] may be created in this directory.")]
+    #[structopt(short = "c", long = "copy", help = "Copy (rather than move) images to output directory, sorted by orientation. Subdirectories [portrait, landscape, square] may be created in this directory.")]
     mv: bool,
-    #[structopt(short, long, help = "Prepend 'portrait', 'landscape', or 'square' to image filenames.")]
+    #[structopt(short, long, help = "Prepend 'portrait', 'landscape', or 'square' to output image filenames.")]
     prefix: bool,
-    #[structopt(short, long, parse(from_occurrences), help = "Increasingly verbose output to stderr specified by adding more flags. i.e. -v -vv -vvv")]
+    #[structopt(short, long, parse(from_occurrences), help = "Increase output verbosity by adding more flags: [-v|-vv|-vvv]")]
     verbose: usize,
     #[structopt(short, long, help = "Do not print anything to stdout or stderr.")]
     quiet: bool,
-    #[structopt(short, long, help = "Do not actually move any files. Implies verbose unless --quiet is provided.")]
+    #[structopt(short, long, help = "Do not actually move any files. Implies verbose unless --quiet is present.")]
     dry_run: bool,
 }
 
 fn main() -> Result<(), Error>{
-    let opt = Opt::from_args();
-    stderrlog::new()
-        .module(module_path!())
-        .quiet(opt.quiet)
-        .verbosity(opt.verbose)
-        .init()
-        .unwrap();
-    
-    debug!("CLI options: {:?}", opt);
+    let opt = init();
 
     if opt.recursive {
-        recursive_rw_loop(opt.directory)?;
+        recursive_rw_loop(opt.input_dir)?;
     } else {
-        rw_loop(opt.directory)?;
+        rw_loop(opt.input_dir)?;
     }
 
     if !opt.quiet { println!("Operation complete!"); }
@@ -49,18 +43,33 @@ fn main() -> Result<(), Error>{
     Ok(())
 }
 
-fn recursive_rw_loop(directory: PathBuf) -> Result<(), Error> {
-    debug!("Recursively walking directory tree starting at {}", directory.display());
-    for dir in WalkDir::new(directory) {
-        debug!("  {}", dir?.path().display());
+/// Get CLI options, initilize logging.
+fn init() -> Opt {
+    let opt = Opt::from_args();
+    stderrlog::new()
+        .module(module_path!())
+        .quiet(opt.quiet)
+        .verbosity(opt.verbose)
+        .init()
+        .unwrap();
+    debug!("CLI options: {:?}", opt);
+    return opt
+}
+
+/// Recursively walk given directory, operating on each image file.
+fn recursive_rw_loop(input_dir: PathBuf) -> Result<(), Error> {
+    debug!("Recursively walking directory tree starting at {}", input_dir.display());
+    for f in WalkDir::new(input_dir) {
+        debug!("{}", f?.path().display());
     }
     Ok(())
 }
 
-fn rw_loop(directory: PathBuf) -> Result<(), Error> {
-    debug!("Directory contents at {}", directory.display());
-    for dir in fs::read_dir(".")? {
-        debug!("  {}", dir?.path().display());
+/// Walk given directory, operating on each image file.
+fn rw_loop(input_dir: PathBuf) -> Result<(), Error> {
+    debug!("Directory contents at {}", input_dir.display());
+    for f in fs::read_dir(".")? {
+        debug!("{}", f?.path().display());
     }
     Ok(())
 }
