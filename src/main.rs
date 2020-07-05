bc3cac944f7fa96da484f9f3b002322ce16631c0
🎷🐛 Lee Ball <lee.cat.ball@gmail.com>
2020-07-04 17:26:12 -0700

    More move impl, but it's still broken

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,7 @@ use clap::arg_enum;
 use image::image_dimensions;
 use failure::Error;
 use std::cmp::Ordering;
-use std::fs;
-use std::fs::File;
-use std::path;
+use std::fs::{create_dir_all, rename};
 use std::path::{Path, PathBuf};
 use structopt::StructOpt;
 use walkdir::WalkDir;
@@ -19,7 +17,7 @@ const IMG_EXTS: [&str; 8] = ["gif", "jpeg", "ico", "png", "tiff", "webp", "bmp",
 arg_enum! {
     #[derive(Debug)]
     enum OverwriteBehavior {
-        Append,
+        Rename,
         Overwrite,
         Skip,
     }
@@ -35,10 +33,10 @@ struct Opt {
     #[structopt(short, long, help = "Recurse into subdirectories.")]
     recursive: bool,
     #[structopt(short = "c", long = "copy", help = "Copy (rather than move) images to output directory, sorted by orientation. Subdirectories [portrait, landscape, square] may be created in this directory.")]
-    mv: bool,
-    #[structopt(short, long, help = "Prepend 'portrait', 'landscape', or 'square' to output image filenames.")]
+    copy: bool,
+    #[structopt(short, long, help = "Prepend 'portrait_', 'landscape_', or 'square_' to output image filenames.")]
     prefix: bool,
-    #[structopt(long, help = "Rename files without moving them, prepending 'tall', 'wide', or 'square' to the filename. If this option is present, ignore -c, -p, and output_dir.")]
+    #[structopt(long, help = "Rename files without moving them, prepending 'portrait_', 'landscape_', or 'square_' to the filename. If this option is present, ignore -c, -p, and output_dir.")]
     rename: bool,
     #[structopt(long, help = "Guess if a file is an image based on file header rather than file extension. Performs more slowly than reading extensions.")]
     read_headers: bool,
@@ -48,15 +46,39 @@ struct Opt {
     quiet: bool,
     #[structopt(short, long, help = "Do not actually move or copy any files. Print files to stdout unless --quiet is present.")]
     dry_run: bool,
-    #[structopt(possible_values = &OverwriteBehavior::variants(), case_insensitive = true, help = "Specify behavior when a file with the same name exists in the output directory. Possible options: [append (adds a number to the end of the filename, keeping both files), overwrite (replace file in destination directory), skip (do not move file, leave in original location.)]")]
+    #[structopt(long, possible_values = &OverwriteBehavior::variants(), default_value = "rename", case_insensitive = true, help = "Specify behavior when a file with the same name exists in the output directory. Possible options: [rename (adds a number to the end of the filename, keeping both files), overwrite (replace file in destination directory), skip (do not move file, leave in original location).]")]
     overwrite: OverwriteBehavior,
+}
+
+struct Img {
+    err: Option<Error>,
+    src: PathBuf,
+    dst: PathBuf,
 }
 
 fn main() -> Result<(), Error> {
     let opts: Opt = init();
+    if opts.dry_run || opts.copy || opts.read_headers || opts.rename { exit_no_impl(); }
+    if !opts.dry_run && !opts.copy && !opts.rename { make_output_orientation_dirs(&opts)?; }
     let src_dest_map = read_files(opts.input_dir, opts.output_dir, opts.recursive);
     debug!("{:?}", src_dest_map);
     if !opts.quiet { println!("Operation complete!"); }
+    Ok(())
+}
+
+fn exit_no_impl() {
+    error!("Not yet implemented.");
+    std::process::exit(1);
+}
+
+fn make_output_orientation_dirs(opts: &Opt) -> Result<(), Error> {
+    let outstr = opts.output_dir.to_str().unwrap_or("");
+    let tall = format!("{}/portrait", outstr);
+    let wide = format!("{}/landscape", outstr);
+    let square = format!("{}/square", outstr);
+    create_dir_all(tall)?;
+    create_dir_all(wide)?;
+    create_dir_all(square)?;
     Ok(())
 }
 
@@ -75,17 +97,31 @@ fn init() -> Opt {
 
 fn move_files(src_dest_map: Vec<(PathBuf, PathBuf)>, overwrite: OverwriteBehavior) -> Result<(), Error> {
     match overwrite {
-        Overwrite => move_files_overwrite(src_dest_map),
-        Append => (),
-        Skip => (),
+        OverwriteBehavior::Overwrite => move_files_quiet_overwrite(src_dest_map),
+        OverwriteBehavior::Rename => {exit_no_impl();},
+        OverwriteBehavior::Skip => {exit_no_impl();},
     }
     Ok(())
 }
 
-fn move_files_overwrite(src_dest_map: Vec<(PathBuf, PathBuf)>) {
-    src_dest_map.iter()
-                .filter_map( |sd| fs::rename(sd.0, sd.1).ok() );
+/// Move files to new destination, suppress all errors, overwrite dest files, return no metadata.
+fn move_files_quiet_overwrite(mut src_dest_map: Vec<(PathBuf, PathBuf)>) -> () {
+    src_dest_map.drain(..)
+                .filter_map( |sd| rename(Path::new(&sd.0), Path::new(&sd.1)).ok() )
+                .collect()
 }
+
+// fn move_files_overwrite(src_dest_map: Vec<(PathBuf, PathBuf)>) {
+//     let mv_errors: Vec<(std::io::Error, &PathBuf, &PathBuf)> = Vec::with_capacity(src_dest_map.len());
+//     let (pass, fail): (Vec<_>, Vec<_>) = src_dest_map
+//         .iter()
+//         .map( |sd| rename(Path::new(&sd.0), Path::new(&sd.1)) )
+//         .partition(Result::is_ok);
+//     let errors: Vec<_> = fail
+//         .iter()
+//         .map(|e| e.as_ref().unwrap_err())
+//         .collect();
+// }
 
 /// Recursively walk input directory, return a vector of image source paths to destination paths.
 fn read_files(input_path: PathBuf, output_path: PathBuf, recursive: bool) -> Vec<(PathBuf, PathBuf)> {
@@ -122,17 +158,17 @@ fn get_src_dest_paths(inpath: &Path, mut outpath: PathBuf) -> Result<(PathBuf, P
     };
     match x.cmp(&y) {
         Ordering::Greater => {
-            outpath.push("wide");
+            outpath.push("portrait_");
             outpath.push(imgfile);
             Ok( (inpath.to_path_buf(), outpath) )
         },
         Ordering::Less => {
-            outpath.push("tall");
+            outpath.push("landscape_");
             outpath.push(imgfile);
             Ok( (inpath.to_path_buf(), outpath) )
         }
         Ordering::Equal => {
-            outpath.push("square");
+            outpath.push("square_");
             outpath.push(imgfile);
             Ok( (inpath.to_path_buf(), outpath) )
         }
